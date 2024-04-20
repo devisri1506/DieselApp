d8132527e2efd51a6f20b2d0a4420e5253c30ecc
Devi Sri <pallothudevisri@gmail.com>
2024-04-20 11:48:58 +0530

    removed unnecessary files

--- App.js
+++ App.js
@@ -3,9 +3,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { app } from './firebase'; // Import Firebase app instance
-import LoginScreen from './android/app/src/screens/auth/loginScreen'; // Correct import
 import EmailSignInScreen from './android/app/src/screens/auth/EmailSignInScreen'; // Correct import
-import EmailSignUpScreen from './android/app/src/screens/auth/EmailSignUpScreen'; // Correct import
 import HomeScreen from './android/app/src/screens/Home/HomeScreen';
 import Transaction from './android/app/src/screens/Home/Transaction';
 
@@ -16,7 +14,6 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="EmailSignIn" component={EmailSignInScreen} />
-        <Stack.Screen name="SignUp" component={EmailSignUpScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Transaction" component={Transaction} />
         {/* Add more screens here if needed */}
